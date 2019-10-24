ccb8557a1f41e8b0a6877ca5be6d77102e32b4a4
Kostya Rekkel <kostarekkel@gmail.com>
2019-10-24 09:14:32 +0100

    1

---
+++ LAB1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#define LED 0
+int main(void)
+{
+printf ("Raspberry Pi blink\n");
+wiringPiSetup ();
+pinMode (LED, OUTPUT);
+for (;;)
+{
+digitalWrite (LED, HIGH);
+delay (500);
+digitalWrite (LED, LOW);
+delay (500);
+}
+return 0;
+}
